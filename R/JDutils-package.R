904023554f87b0f4ac809662cc6e68e96333bf80
Davies, Jenny <jenny.davies@mhra.gov.uk>
2025-07-01 16:32:07 +0100

    corrected notes re non visible bindings for global variables

--- R/JDutils-package.R
+++ R/JDutils-package.R
@@ -2,6 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom rlang .data
 #' @importFrom utils read.csv
 ## usethis namespace: end
 NULL
--- R/calculate_working_days.R
+++ R/calculate_working_days.R
@@ -8,15 +8,14 @@
 #'
 #' @examples
 #' calculate_working_days(start = "2024-01-01", end = "2024-12-31")
-calculate_working_days <- function(start = year_start,
-                                   end = year_end) {
+calculate_working_days <- function(start, end) {
   weekdays <- data.frame(dates = seq(lubridate::ymd(start), 
                                      lubridate::ymd(end), by = "day")) |> 
-    dplyr::mutate(day = lubridate::wday(dates, week_start = 1),
-                  month_start = lubridate::floor_date(dates, "month")) |> 
-    dplyr::filter(!day %in% c(6, 7)) |> 
-    dplyr::group_by(month_start) |> 
-    dplyr::summarise(n_weekdays = dplyr::n_distinct(dates))
+    dplyr::mutate(day = lubridate::wday(.data$dates, week_start = 1),
+                  month_start = lubridate::floor_date(.data$dates, "month")) |> 
+    dplyr::filter(!.data$day %in% c(6, 7)) |> 
+    dplyr::group_by(.data$month_start) |> 
+    dplyr::summarise(n_weekdays = dplyr::n_distinct(.data$dates))
   
   bank_holiday_list <- readLines("https://www.gov.uk/bank-holidays/england-and-wales.ics") |> 
     stringr::str_subset("^DTSTART") |> 
@@ -24,18 +23,18 @@ calculate_working_days <- function(start = year_start,
     lubridate::ymd()
   
   bank_holidays <- data.frame(bh_dates = bank_holiday_list) |> 
-    dplyr::filter(bh_dates >= start &
-                    bh_dates <= end) |> 
-    dplyr::mutate(month_start = lubridate::floor_date(bh_dates, "month")) |> 
-    dplyr::group_by(month_start) |> 
-    dplyr::summarise(n_bankholidays = dplyr::n_distinct(bh_dates))
+    dplyr::filter(.data$bh_dates >= start &
+                    .data$bh_dates <= end) |> 
+    dplyr::mutate(month_start = lubridate::floor_date(.data$bh_dates, "month")) |> 
+    dplyr::group_by(.data$month_start) |> 
+    dplyr::summarise(n_bankholidays = dplyr::n_distinct(.data$bh_dates))
   
   working_days <- weekdays |> 
     dplyr::left_join(bank_holidays, by = "month_start") |> 
-    dplyr::mutate(n_bankholidays = ifelse(is.na(n_bankholidays), 0, n_bankholidays),
-                  n_working_days = n_weekdays - n_bankholidays,
-                  year_total = sum(n_working_days)) |> 
-    dplyr::select(month_start, n_working_days, year_total)
+    dplyr::mutate(n_bankholidays = ifelse(is.na(.data$n_bankholidays), 0, .data$n_bankholidays),
+                  n_working_days = .data$n_weekdays - .data$n_bankholidays,
+                  year_total = sum(.data$n_working_days)) |> 
+    dplyr::select(.data$month_start, .data$n_working_days, .data$year_total)
   
   return(working_days)
 }
