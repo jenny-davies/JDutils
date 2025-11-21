0c81857056bd6ee15964a6849cedd6237fb8b924
Davies, Jenny <jenny.davies@mhra.gov.uk>
2025-11-21 15:13:45 +0000

    patch to make set_debugging_options optional if searcher package LLM functions not available

--- R/set_debugging_options.R
+++ R/set_debugging_options.R
@@ -8,8 +8,27 @@
 #' @examples
 #' set_debugging_options()
 set_debugging_options <- function() {
+  if (!requireNamespace("searcher", quietly = TRUE)) {
+    warning("Package 'searcher' not installed; set_debugging_options will not be enabled.")
+    return(invisible(NULL))
+  }
+  
+  exports <- tryCatch(getNamespaceExports("searcher"), error = function(e) character(0))
+  if (!all(c("ai_prompt", "ask_chatgpt") %in% exports)) {
+    warning("searcher does not export ai_prompt and/or ask_chatgpt; set_debugging_options will not be enabled.")
+    return(invisible(NULL))
+  }
+  
+  ai_prompt <- getExportedValue("searcher", "ai_prompt")
+  ask_chatgpt <- getExportedValue("searcher", "ask_chatgpt")
+  
+  if (!is.function(ai_prompt) || !is.function(ask_chatgpt)) {
+    warning("searcher exported objects are not functions; set_debugging_options will not be enabled.")
+    return(invisible(NULL))
+  }
+  
   options(error = function() {
-    searcher::ai_prompt("debugging")
-    searcher::ask_chatgpt()
+    ai_prompt("debugging")
+    ask_chatgpt()
   })
 }
